eaa8e1032da3fe5a98d32589e4863bace4fdcc27
Robert J <robertjewell808@gmail.com>
2019-07-10 17:15:58 -0400

    Update to postgres; Fix transaction js

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_19_175349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "transactions", force: :cascade do |t|
     t.string "desc"
     t.decimal "amount"
